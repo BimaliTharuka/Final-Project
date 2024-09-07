7e2087a5366d0f1e9171a8cf0e5badac63b74fc0
Nethmi Angelina <128815019+Angelina700@users.noreply.github.com>
2024-09-08 01:50:15 +0530

    welcome dashboard by nethmi

--- SRMS/resources/views/welcome.blade.php
+++ SRMS/resources/views/welcome.blade.php
@@ -211,9 +211,9 @@
           <ul>
               @if (Route::has('login'))
               @auth
-                                    <a
-                                        href="{{ route('admin.dashboard') }}"
-                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                      <a
+                             href="{{ route('admin.dashboard') }}"
+                              class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                     >
                                         Dashboard
                                     </a>
